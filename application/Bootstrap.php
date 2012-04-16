261a8abb2f2b8d01d5c1d4b0a6c827e0ce3cd1f8
Joeri <joeri@youniware.com>
2012-04-16 20:14:33 +0200

    changing the route

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,6 +9,33 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         Zend_Registry::set('db', $db);
     }
     
+    public function _initMenu(){
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+        $view = $layout->getView();
+        
+        $model = new Application_Model_Page();
+        $pages = $model->getMenu('nl_BE');
+        
+        $container = new Zend_Navigation();
+        
+        foreach ($pages as $page){
+            $menu = new Zend_Navigation_Page_Mvc(array(
+                'label'         => $page['title'],
+                //'controller'    => strtolower($page['title']),
+                //'action'        => 'index',
+                'route'        => 'page',
+                'params'      => array('titleUrl' => $page['titleURL'])
+            ));
+            $container->addPage($menu);
+        }
+        
+        //container toevoegen aan de nav
+        $view->navigation($container);
+        
+        //return $container;
+    }
+    
     /**
      * Creates all custom routers
      * 
@@ -24,32 +51,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $router->addRoute('page', new Zend_Controller_Router_Route('pagina/:titleUrl', array('controller' => 'page', 'action' => 'index')));
     
         return $router;
-    }
-    
-    public function _initMenu(){
-        $this->bootstrap('layout');
-        $layout = $this->getResource('layout');
-        $view = $layout->getView();
-        
-        $model = new Application_Model_Page();
-        $pages = $model->getMenu('nl_BE');
-        
-        $container = new Zend_Navigation();
-        
-        foreach ($pages as $page){
-            $menu = new Zend_Navigation_Page_Mvc(array(
-                'label'         => $page['title'],
-                'controller'    => strtolower($page['title']),
-                'action'        => 'index',
-                'params'      => array('titleUrl' => $page['titleURL'])
-            ));
-            $container->addPage($menu);
-        }
-        
-        //container toevoegen aan de nav
-        $view->navigation($container);
-        
-        //return $container;
     }
 
 }
