de9f5a7fba139e82c23ae3395733c6f177459db7
Joeri <joeri@youniware.com>
2012-04-16 19:57:45 +0200

    create page controller

---
+++ application/controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+class PageController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        echo '<pre>';
+        print_r($this->_getAllParams());
+        echo '</pre>';
+        die();
+    }
+
+
+}
+
