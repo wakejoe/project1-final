c7eb2bc5d87d33ac3c6ba78ae878f42a01eb477f
Joeri <joeri@youniware.com>
2012-04-24 21:34:54 +0200

    start adminpage

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -19,81 +19,9 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $this->bootstrap('layout');
         $layout = $this->getResource('layout');
         $view = $layout->getView();
-        $view->addHelperPath(realpath(APPLICATION_PATH . '/views/helpers'), 'Application_View_Helper');
-        $model = new Application_Model_Page();
-        $pages = $model->getMenu('nl_BE');
-        
-        $container = new Zend_Navigation();
-        
-        foreach ($pages as $page){
-            $menu = new Zend_Navigation_Page_Mvc(array(
-                'label'         => $page['title'],
-                //'controller'    => strtolower($page['title']),
-                //'action'        => 'index',
-                'route'        => 'page',
-                'params'      => array('titleUrl' => $page['titleURL'])
-            ));
-            $container->addPage($menu);
-        }
-        
-        //container toevoegen aan de nav
-        $view->navigation($container);
-        
-        //return $container;
+        $view->addHelperPath(realpath(APPLICATION_PATH . '/views/helpers'), 'Application_View_Helper');        
     }
     
-    protected function _initTranslate(){
-        /*$locale = new Zend_Locale('nl_BE'); //normaal in de prédispatch anders kan je de taal niet uitlezen
-        Zend_Registry::set('Zend_Locale', $locale);
-        
-        $translate = new Zend_Translate('array', array('yes' => 'ja'), $locale);
-        
-        $model = new Application_Model_Translate();
-        $translations = $model->getTranslationByLocale($locale);
-        
-        foreach ($translations as $translation){
-            $t = array($translation->tag => $translation->translation);
-            $translate->addTranslation($t, $locale);
-        }
-        
-        Zend_Registry::set('Zend_Translate', $translate);
-        
-        
-        $this->_request->getParam('lang');
-        echo '<pre>';
-        print_r($req);
-        echo '</pre>';
-        die();*/
-        $this->bootstrap('frontController');
-        $front = $this->getResource('frontController');
-        $front->setRequest(new Zend_Controller_Request_Http());
-
-        $request = $front->getRequest();
-        Wakejoe_Translate_Translate::preDispatch($request);
-        
-    }
-
-
-    /**
-     * Creates all custom routers
-     * 
-     * @param array $options 
-     * @return Zend_Controller_Router_Route
-     */
-    /*public function _initRouter(array $options = null){
-        
-        // get the router
-        $router = $this->getResource('frontController')->getRouter();
-        
-        // add custom route
-        $router->addRoute('page',
-                new Zend_Controller_Router_Route('pagina/:titleUrl', array(
-                    'controller' => 'page',
-                    'action'     => 'index'
-                )));
-    
-        return $router;
-    }*/
     public function _initRouter(array $options = null){
         
         // get the router
@@ -117,6 +45,14 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $router->addRoute('page',
                 new Zend_Controller_Router_Route(':lang/pagina/:titleUrl', array(
                     'controller' => 'page',
+                    'action'     => 'index'
+                )));
+        
+        // add admin route
+        $router->addRoute('admin',
+                new Zend_Controller_Router_Route(':module/:controller/:action', array(
+                    'module'     => 'admin',
+                    'controller' => 'index',
                     'action'     => 'index'
                 )));
             
