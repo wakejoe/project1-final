a8d965d83547388073f77dd46f1f4973a2a0ed64
Joeri <joeri@youniware.com>
2012-05-07 19:44:10 +0200

    start of auth

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Zend_Controller_Action
     
     public function loginAction()
     {
-        $this->view->form = $this->view->loginFormulier();
+        $this->view->form = New Application_Form_Users();
         
         // controle en mail versturen
         if ($this->getRequest()->isPost())
@@ -25,13 +25,32 @@ class UserController extends Zend_Controller_Action
             if ($this->view->form->isValid($postParams)){
                 $params = $this->view->form->getValues();
                 
-                echo '<pre>';
-                print_r($params);
-                echo '</pre>';
-                die();
+                $auth = Zend_Auth::getInstance();
+                
+                $authAdapter = new Zend_Auth_Adapter_DbTable(Zend_Registry::get('db'));
+                
+                $authAdapter->setTableName('users')
+                            ->setIdentityColumn('username')
+                            ->setCredentialColumn('password')
+                            ->setIdentity($value)
+                            ->setCredential($credential);
+                
+                $result = $auth->authenticate($authAdapter);
+                
+                if ($result->isValid()){
+                    
+                    $identity = Zend_Auth::getInstance()->getIdentity();
+                    echo 'ingelogd als' . $identity;
+                    
+                } else {
+                    
+                    foreach($result->getMessages() as $message) {
+                        
+                    }
+                    
+                }
       
             }else{
-                
                 die('alles invullen!');
                 
            }
